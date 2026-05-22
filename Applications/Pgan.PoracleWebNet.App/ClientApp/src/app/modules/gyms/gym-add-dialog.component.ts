8c8e537f72fedd1783e34894343b616bdc75b7ce
ReuschelCGN <82573872+ReuschelCGN@users.noreply.github.com>
2026-05-22 16:11:49 +0200

    Update gym-add-dialog.component.ts

--- Applications/Pgan.PoracleWebNet.App/ClientApp/src/app/modules/gyms/gym-add-dialog.component.ts
+++ Applications/Pgan.PoracleWebNet.App/ClientApp/src/app/modules/gyms/gym-add-dialog.component.ts
@@ -79,7 +79,7 @@ export class GymAddDialogComponent {
   ];
 
   getGymIcon(team: number): string {
-    if (team === 4) return `https://raw.githubusercontent.com/whitewillem/PogoAssets/main/uicons/gym/${team}.png`;
+    if (team === 4) return `https://raw.githubusercontent.com/whitewillem/PogoAssets/main/uicons/gym/0.png`;
     else return `https://raw.githubusercontent.com/whitewillem/PogoAssets/main/uicons/gym/${team}.png`;
   }
 
