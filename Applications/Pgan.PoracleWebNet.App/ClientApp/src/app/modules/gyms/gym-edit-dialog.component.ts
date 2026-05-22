d5bed2e4fbb2c3d753c4c8fcbda19e1d648cc30d
ReuschelCGN <82573872+ReuschelCGN@users.noreply.github.com>
2026-05-22 16:11:15 +0200

    Update gym-edit-dialog.component.ts

--- Applications/Pgan.PoracleWebNet.App/ClientApp/src/app/modules/gyms/gym-edit-dialog.component.ts
+++ Applications/Pgan.PoracleWebNet.App/ClientApp/src/app/modules/gyms/gym-edit-dialog.component.ts
@@ -63,8 +63,8 @@ export class GymEditDialogComponent {
   saving = signal(false);
   selectedGymId = signal<string | null>(this.data.gymId);
   getGymIcon(): string {
-    if (this.data.team === 4) this.data.team = 0;
-    return `https://raw.githubusercontent.com/whitewillem/PogoAssets/main/uicons/gym/${this.data.team}.png`;
+    if (this.data.team === 4) return `https://raw.githubusercontent.com/whitewillem/PogoAssets/main/uicons/gym/${this.data.team}.png`;
+    else return `https://raw.githubusercontent.com/whitewillem/PogoAssets/main/uicons/gym/${this.data.team}.png`;
   }
 
   getTeamName(team: number): string {
