868d103fca919cfb4f3872688c74e57ea2490b1e
ReuschelCGN <82573872+ReuschelCGN@users.noreply.github.com>
2026-05-22 16:06:35 +0200

    Update gym-list.component.ts

--- Applications/Pgan.PoracleWebNet.App/ClientApp/src/app/modules/gyms/gym-list.component.ts
+++ Applications/Pgan.PoracleWebNet.App/ClientApp/src/app/modules/gyms/gym-list.component.ts
@@ -158,8 +158,8 @@ export class GymListComponent implements OnInit {
   }
 
   getGymIcon(team: number): string {
-    if (team === 4) team = 0;
-    return `https://raw.githubusercontent.com/whitewillem/PogoAssets/main/uicons/gym/${team}.png`;
+    if (team === 4) return `https://raw.githubusercontent.com/whitewillem/PogoAssets/main/uicons/gym/0.png`;
+    else return `https://raw.githubusercontent.com/whitewillem/PogoAssets/main/uicons/gym/${team}.png`;
   }
 
   getTeamColor(team: number): string {
